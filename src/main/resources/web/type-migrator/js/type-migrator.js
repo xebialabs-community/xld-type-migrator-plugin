b17ddc23754d9e3d5720880f65df267b4267f553
droberts2013 <droberts@xebialabs.com>
2016-03-01 21:03:51 -0500

    Refactor for deployables and deployeds

--- src/main/resources/web/type-migrator/js/type-migrator.js
+++ src/main/resources/web/type-migrator/js/type-migrator.js
@@ -59,14 +59,14 @@ function refresh() {
     loadApplications();
 }
 
-function migrate() {
+function migrateDeployables() {
   p1 = $('#obsoleteType').val();
   p2 = $('#applicationPath').val();
   p3 = $('#oldVerNum').val();
   p4 = $('#newVerNum').val();
   $.ajax({
     datatype: "json",
-    url: "/api/extension/type-migrator/migrate?p1=" + p1 + "&p2=" + p2 + "&p3=" + p3 + "&p4=" + p4,
+    url: "/api/extension/type-migrator/migrateDeployables?p1=" + p1 + "&p2=" + p2 + "&p3=" + p3 + "&p4=" + p4,
     crossDomain: true,
     beforeSend: function(xhr) {
       var base64 = parent.getAuthToken();
